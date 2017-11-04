798b42aec739a9e00d83e5bf8d2f4681cf220382
speexy <jennifer.meyer@posteo.de>
2017-11-04 13:52:23 +0100

    show that communication with server works in principal

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,21 +1,48 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+    render() {
+
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function() {
+            if (this.readyState === 4 && this.status === 200) {
+                console.log(JSON.parse(this.response).jwt);
+                sessionStorage.setItem('jwt', JSON.parse(this.response).jwt);
+            }
+        };
+        xhr.open('POST', "/user");
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.send(JSON.stringify({username: "doctor", password: "password"}))
+
+        function beforesend(request) {
+            var jwt = "bearer " + sessionStorage.getItem("jwt");
+            request.setRequestHeader('authorization', jwt)
+        }
+        var xhr2 = new XMLHttpRequest();
+        xhr2.onreadystatechange = function() {
+            if (this.readyState === 4 && this.status === 200) {
+                console.log(this.response);
+            }
+        };
+        xhr2.open('GET', "/token");
+        beforesend(xhr2);
+        xhr2.send();
+
+        return (<div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <h1 className="App-title">Welcome, this is React</h1>
+            </header>
+            <p className="App-intro">
+                To get started, edit
+                <code>src/App.js</code>
+                and save to reload.
+            </p>
+        </div>);
+    }
 }
 
 export default App;
