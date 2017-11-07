3be2a978321ecb12e81b87b8015d504f834a20d4
speexy <jennifer.meyer@posteo.de>
2017-11-07 12:09:30 +0100

    fix markup mistake and property name

--- ui/src/Login.jsx
+++ ui/src/Login.jsx
@@ -3,16 +3,16 @@ import React from "react";
 function Login () {
     return (<form method="post">
         <div className="form-group col-6">
-            <label for="inputUsername">
+            <label htmlFor="inputUsername">
                 Username
             </label>
             <input type="text" className="form-control" id="inputUsername" placeholder="Enter username" name="username"/>
+        </div>
         <div className="form-group col-6">
-            <label for="inputPassword">
+            <label htmlFor="inputPassword">
                 Password
             </label>
             <input type="text" className="form-control" id="inputPassword" placeholder="Password" name="password"/>
-        </div>
         </div>
     </form>)
 }
