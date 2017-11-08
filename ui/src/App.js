616ca1ec46e40073b6a6da8054e707cfe4a9da86
speexy <jennifer.meyer@posteo.de>
2017-11-08 16:53:01 +0100

    make sure state and sessionStorage correspond. otherwise logout button breaks on refresh

--- ui/src/App.js
+++ ui/src/App.js
@@ -5,19 +5,25 @@ import Twilio from "./Twilio"
 
 
 
-const initialState = {
-    loggedIn: sessionStorage.getItem("loggedIn"),
-    identity: sessionStorage.getItem("identity"),
-    token: sessionStorage.getItem("token")
-}
+
 
 class App extends Component {
-    state = initialState;
+    getState= function(){
+        return {
+              loggedIn: sessionStorage.getItem("loggedIn"),
+              identity: sessionStorage.getItem("identity"),
+              token: sessionStorage.getItem("token")
+          }
+    }
+    state = this.getState();
+
     login = Api.getTwilio.bind(this);
+
     logout = function(){
         sessionStorage.clear();
-        this.setState(initialState);
+        this.setState(this.getState());
     }.bind(this)
+
 
     render() {
         return this.state.loggedIn ? <Twilio identity={this.state.identity} token={this.state.token} logout={this.logout}/> : <Login login={this.login}/>
