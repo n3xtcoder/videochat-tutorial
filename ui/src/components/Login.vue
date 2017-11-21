de2f4f9d33cab534fc847f720e445a3b5ca35c15
jonfm <jon@jonfm.co.uk>
2017-11-21 15:38:27 +0100

    Standalone login component

---
+++ ui/src/components/Login.vue
@@ -0,0 +1,31 @@
+<template>
+  <form v-on:submit.prevent="onLogin">
+    <div v-if="error">{{error}}</div>
+    <label>Username</label><input type="text" v-model.trim="username"/>
+    <label>Password</label><input type="password" v-model.trim="password"/>
+    <button type="submit" v-bind:disabled="isLoginDisabled()">Login</button>
+  </form>
+</template>
+
+<script>
+  import { login } from '../utils/client'
+
+  export default {
+    name: 'Login',
+    data () {
+      return {
+        username: '',
+        password: '',
+        error: ''
+      }
+    },
+    methods: {
+      isLoginDisabled: function () {
+        return !(this.username && this.password)
+      },
+      onLogin: function () {
+        login(this.username, this.password)
+      }
+    }
+  }
+</script>
