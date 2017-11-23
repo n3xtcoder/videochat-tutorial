5998768573d081e065e472772b404192bd2c1964
Rico <e.cerroni@gmail.com>
2017-11-23 13:37:07 +0100

    add header && footer for layout

---
+++ ui/src/components/layout/_head.vue
@@ -0,0 +1,41 @@
+<template>
+   <div>
+     <header>
+       <p id="header-content">Prototype</p>
+     </header>
+   </div>
+</template>
+
+<script>
+
+export default {
+  name: 'head',
+  // props: [],
+  mounted() {
+    //
+  },
+
+  data() {
+    return {
+      //
+    };
+  },
+
+  methods: {
+    //
+  },
+};
+</script>
+
+<style scoped>
+header {
+  margin-bottom: 60px;
+  padding: 10px;
+  border-bottom: 3px solid #000;
+}
+#header-content {
+  font-weight: 900;
+  font-size: large;
+  color: #42b983;
+}
+</style>
