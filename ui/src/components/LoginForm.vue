f39060d49b05bd924bce651fff85e5e0af76625f
Vince Varga <vvarga@schoen-kliniken.de>
2017-12-11 14:46:02 +0100

    Add basic login form to /login path.

---
+++ ui/src/components/LoginForm.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <h1>VideoChat Tutorial</h1>
+    <h2>Login</h2>
+    <form @submit.prevent="login">
+      <label>
+        Username
+        <input v-model="username" type="text"  placeholder="Your username">
+      </label>
+      <label>
+        Password
+        <input v-model="password" type="password"  placeholder="Your password">
+      </label>
+      <button type="submit">
+        Login
+      </button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data() {
+    return {
+      username: '',
+      password: '',
+    };
+  },
+  methods: {
+    login() {
+      console.log(`TODO: POST request with username: ${this.username} and password: ${this.password}`);
+    },
+  },
+};
+</script>
+
+<style scoped>
+label {
+  display: block;
+}
+</style>
--- ui/src/router/index.js
+++ ui/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from '@/components/Home';
+import LoginForm from '@/components/LoginForm';
 
 Vue.use(Router);
 
@@ -11,6 +12,11 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: Home,
+    },
+    {
+      path: '/login',
+      name: 'LoginForm',
+      component: LoginForm,
     },
   ],
 });
