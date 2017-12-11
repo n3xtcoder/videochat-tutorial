47423607036285b84c1fb91ad837d25aea56c81b
Darío Blanco <dblancoit@gmail.com>
2017-12-11 14:37:45 +0100

    Create basic Home page with bootstrap vue

---
+++ ui/src/components/Home.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="home">
+    <b-jumbotron :header="title"
+                 :lead="msg">
+      <b-button size="lg" variant="primary" to="/video">Video</b-button>
+    </b-jumbotron>
+    <b-container>
+      <b-row>
+        <b-col lg="6">
+          <h4>Powered by Vue</h4>
+          <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
+
+          <h4>Delivered by Schoen Digital</h4>
+          <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
+        </b-col>
+
+        <b-col lg="6">
+          <h4>Fast is our DNA</h4>
+          <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
+
+          <h4>Reliability in mind</h4>
+          <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data() {
+    return {
+      title: 'Videochat tutorial',
+      msg: "A videochat using Vue.js, written for MindDoc's and N3XTCODER's workshop on Vue.js and React.",
+    };
+  },
+};
+</script>
+
+<style>
+</style>
