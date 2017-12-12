5321ab9d8c2745518aa2572c6a04920be74714dd
Darío Blanco <dblancoit@gmail.com>
2017-12-12 14:52:16 +0100

    Add video route

--- ui/src/router/index.js
+++ ui/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from '@/components/Home';
+import Video from '@/components/Video';
 
 Vue.use(Router);
 
@@ -11,6 +12,11 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: Home,
+    },
+    {
+      path: '/video',
+      name: 'Video',
+      component: Video,
     },
   ],
 });
