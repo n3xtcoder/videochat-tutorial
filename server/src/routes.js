268e5ab538537bfac9c4fc38a238629559a74461
Darío Blanco <dblancoit@gmail.com>
2017-12-13 23:27:39 +0100

    Rearrange endpoints in routes.js

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,11 +1,68 @@
-const {
-  passport, authenticate, isAuthenticated, userInfo,
-} = require('./auth');
+const { passport, isAuthenticated } = require('./auth');
+const jwt = require('jsonwebtoken');
+const users = require('./users');
+
+const apiSecret = process.env.JWT_API_SECRET;
+
+/**
+ * Uses JWT authentication
+ * @param{object} req the http request
+ * @param{object} res the http response
+ * @return{undefined}
+ */
+function authenticate(req, res) {
+  const { username, password } = req.body;
+
+  users.findByUsername(username, (err, user) => {
+    // handle errors
+    if (err) {
+      console.error(err);
+      return res.status(400).send({
+        message: 'bad request',
+      });
+    }
+
+    // not found
+    if (!user) {
+      return res.status(404).send({
+        message: 'user not found',
+      });
+    }
+
+    // check password
+    if (!(user.password === password)) {
+      return res.status(401).send({
+        message: 'authentication failed',
+      });
+    }
+
+    // create signed JWT
+    return res.status(200).send({
+      jwt: jwt.sign(user, apiSecret, { expiresIn: 60 * 60 * 24 }),
+    });
+  });
+}
+
+/**
+ * Provides extra user information
+ * @param{object} req the http request
+ * @param{object} res the http response
+ * @return{object} the user object as json
+ */
+function userInfo(req, res) {
+  const expiresIn = new Date(req.user.exp * 1000);
+  return res.status(200).send({
+    id: req.user.id,
+    username: req.user.username,
+    displayName: req.user.displayName,
+    role: req.user.role,
+    expiresIn: expiresIn.toISOString(),
+  });
+}
 
 const { AccessToken } = require('twilio').jwt;
 
 const { VideoGrant, ChatGrant } = AccessToken;
-
 const {
   TWILIO_ACCOUNT_SID, TWILIO_API_SID, TWILIO_API_SECRET, TWILIO_CHAT_SERVICE_SID,
 } = process.env;
@@ -14,7 +71,7 @@ const {
  * Generate a Twilio Access Token for the provided identity
  * @param{object} req the http request
  * @param{object} res the http response
- * @return{undefined}
+ * @return{undefined} the identity and jwt token as json
  */
 function createToken(req, res) {
   const identity = req.user.username;
@@ -41,7 +98,7 @@ function createToken(req, res) {
   }
 
   // Serialize the token to a JWT string and include it in a JSON response.
-  res.status(200).send({
+  return res.status(200).send({
     identity: token.identity,
     token: token.toJwt(),
   });
