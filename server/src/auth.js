8192a90174d9b0b0050c7965086521f8dfac83bf
Darío Blanco <dblancoit@gmail.com>
2017-12-13 23:27:39 +0100

    Rearrange endpoints in routes.js

--- server/src/auth.js
+++ server/src/auth.js
@@ -1,8 +1,6 @@
 const passport = require('passport');
 const JwtStrategy = require('passport-jwt').Strategy;
 const { ExtractJwt } = require('passport-jwt');
-const jwt = require('jsonwebtoken');
-const users = require('./users');
 
 const apiSecret = process.env.JWT_API_SECRET;
 
@@ -16,47 +14,3 @@ passport.use(new JwtStrategy({
 module.exports.passport = passport;
 
 module.exports.isAuthenticated = passport.authenticate('jwt', { session: false });
-
-module.exports.authenticate = function authenticate(req, res) {
-  const { username, password } = req.body;
-
-  users.findByUsername(username, (err, user) => {
-    // handle errors
-    if (err) {
-      console.error(err);
-      return res.status(400).send({
-        message: 'bad request',
-      });
-    }
-
-    // not found
-    if (!user) {
-      return res.status(404).send({
-        message: 'user not found',
-      });
-    }
-
-    // check password
-    if (!(user.password === password)) {
-      return res.status(401).send({
-        message: 'authentication failed',
-      });
-    }
-
-    // create signed JWT
-    return res.status(200).send({
-      jwt: jwt.sign(user, apiSecret, { expiresIn: 60 * 60 * 24 }),
-    });
-  });
-};
-
-module.exports.userInfo = function userInfo(req, res) {
-  const expiresIn = new Date(req.user.exp * 1000);
-  return res.status(200).send({
-    id: req.user.id,
-    username: req.user.username,
-    displayName: req.user.displayName,
-    role: req.user.role,
-    expiresIn: expiresIn.toISOString(),
-  });
-};
