22498ebe32a996e9a90f43b9eae6c66ed40ec789
Vince Varga <vvarga@schoen-kliniken.de>
2017-12-13 15:16:29 +0100

    Add localStorage mock for unit tests.

--- ui/test/unit/setup.js
+++ ui/test/unit/setup.js
@@ -1,3 +1,30 @@
-import Vue from 'vue'
+import Vue from 'vue';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+// TODO: Can I use ES6?
+// TODO: Linter should run on this.
+
+var localStorage = (function() {
+    var store = {};
+
+    return {
+        getItem: function(key) {
+            return store[key] || null;
+        },
+        setItem: function(key, value) {
+            store[key] = value.toString();
+        },
+        clear: function() {
+            store = {};
+        },
+        removeItem: function(key) {
+          delete store[key];
+        }
+    };
+
+})();
+
+Object.defineProperty(window, 'localStorage', {
+     value: localStorage
+});
