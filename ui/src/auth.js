79b2ff6338da6c56ee9c69a7b9dc37aeab68fc74
Darío Blanco <dblancoit@gmail.com>
2017-12-13 20:02:13 +0100

    Set getTwilioToken endpoint

--- ui/src/auth.js
+++ ui/src/auth.js
@@ -34,9 +34,19 @@ function assertAuthenticated() {
   return new Promise((resolve, reject) => {
     const t = jwt.get();
     if (t === null) reject('No JWT available.');
-    // TODO: We could create an endpoint, that tells us if the JWT is valid
-    // without creating a token with Twilio.
-    // As we don't have it yet, let's make a call to /api/token with the JWT.
+    axios.get('/api/info', { headers: { Authorization: `Bearer ${t}` } })
+      .then(resolve)
+      .catch(() => {
+        jwt.remove();
+        reject(`JWT ${t} was invalid.`);
+      });
+  });
+}
+
+function getTwilioToken() {
+  return new Promise((resolve, reject) => {
+    const t = jwt.get();
+    if (t === null) reject('No JWT available.');
     axios.get('/api/token', { headers: { Authorization: `Bearer ${t}` } })
       .then(resolve)
       .catch(() => {
@@ -48,6 +58,7 @@ function assertAuthenticated() {
 
 export default {
   assertAuthenticated,
+  getTwilioToken,
   authenticate,
   jwt,
 };
